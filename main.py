c7dbac3dfdf966f3c63be11b084348454ce98f71
university bot j <university-bot-j@infra-bedrock-490110-e4.iam.gserviceaccount.com>
2026-03-23 14:16:43 +0300

    add bot.py module

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import threading
+print("✅ main.py imported successfully")
 import os
 from bot import init_bot
 from web import app
 
+print("🚀 calling init_bot")
 if __name__ == "__main__":
     bot_thread = threading.Thread(target=init_bot, daemon=True)
     bot_thread.start()
