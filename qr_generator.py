17461c55d6f1dcb40c26347e49c0049736b743e5
janahmhashem-web <janahmhashem@gmail.com>
2026-04-01 07:21:27 +0300

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -1,15 +1,19 @@
 import qrcode
+import io
 import base64
-from io import BytesIO
 
 class QRGenerator:
     @staticmethod
-    def generate_qr(data):
-        qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    def generate_qr(data, box_size=10, border=4):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=box_size,
+            border=border,
+        )
         qr.add_data(data)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
-        buffered = BytesIO()
+        buffered = io.BytesIO()
         img.save(buffered, format="PNG")
-        img_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
-        return img_base64
+        return base64.b64encode(buffered.getvalue()).decode('utf-8')
