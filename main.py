ddb041538413f7fe67e10c013dd4cf28d657163a
janahmhashem-web <janahmhashem@gmail.com>
2026-04-09 20:28:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1587,11 +1587,11 @@ def register_transaction():
                     <form id="transactionForm" enctype="multipart/form-data">
                         <div class="form-group">
                             <label class="required">الاسم الثلاثي</label>
-                            <input type="text" id="name" name="name" required placeholder="مثال: أحمد محمد علي">
+                            <input type="text" id="name" name="name" required placeholder="اكتب ...">
                         </div>
                         <div class="form-group">
                             <label class="required">رقم الهاتف</label>
-                            <input type="text" id="phone" name="phone" required placeholder="07712345678">
+                            <input type="text" id="phone" name="phone" required placeholder="اكتب ...">
                         </div>
                         <div class="form-group">
                             <label class="required">الوظيفة</label>
